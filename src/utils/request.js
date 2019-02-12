e6a05406f4a040dd0c5efc1472becabb91b86ab5
wales <wales520@gmail.com>
2019-02-12 14:32:46 +0800

    Auto stash before revert of "變數更名"

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import store from '@/store'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.BASE_API, // api 的 base_url
+  baseURL: process.env.VUE_APP_BASE_API, // api 的 base_url
   timeout: 5000 // request timeout
 })
 // request interceptor
